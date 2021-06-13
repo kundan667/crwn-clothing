ed4e5ed60bef67500cf159ba64fb5bfd80f4ae4c
Kundan choudhary <zrkundan@gmail.com>
2021-06-13 22:08:25 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={ HomePage }></Route>
+        <Route path='/shop/hats' component={ HatsPage }></Route>
+      </Switch>
     </div>
   );
 }
