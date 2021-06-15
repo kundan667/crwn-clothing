34628dc6ff9ca54014b6b306d0b9331f0732ef7a
Kundan choudhary <zrkundan@gmail.com>
2021-06-15 21:23:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { Component } from 'react'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
@@ -37,15 +37,23 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={ HomePage }></Route>
           <Route path='/shop' component={ ShopPage }></Route>
-          <Route path='/signin' component={ SignInAndSignUpPage }></Route>
+          <Route exact path='/signin' render={ () => this.props.currentUser ? 
+            (<Redirect to='/'/>) 
+            : 
+            <SignInAndSignUpPage /> }>
+          </Route>
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
