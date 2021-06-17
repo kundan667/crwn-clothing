e5020d94037ba424106a3624ddfd1fbcc50e6461
Kundan choudhary <zrkundan@gmail.com>
2021-06-17 09:39:55 +0530

    cart add item remove item implemented

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,6 +1,4 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    console.log("called:",cartItems);
-    console.log("cartItemToAdd:",cartItemToAdd);
     const existingCartItem = cartItems.find(
         cartItem => cartItem.id === cartItemToAdd.id
     );
@@ -11,8 +9,24 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
             ? { ...cartItem, quantity: cartItem.quantity + 1 }
             : cartItem
         )
-        
+    }
+    return [ ...cartItems, { ...cartItemToAdd, quantity: 1 } ]
+}
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    );
+
+    if(existingCartItem.quantity === 1){
+        return cartItems.filter( cartItem => cartItem.id !== cartItemToRemove.id )
     }
 
-    return [ ...cartItems, { ...cartItemToAdd, quantity: 1 } ]
+    return cartItems.map( cartItem =>
+        cartItem.id === cartItemToRemove.id
+        ?
+        { ...cartItem, quantity: cartItem.quantity - 1 }
+        :
+        cartItem 
+    )
 }
